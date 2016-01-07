54c2b9f6769efd3fa5a01b10c50ea93828fda80a
parora86 <parora.cpa@outlook.com>
2016-01-07 04:58:58 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+require "prime"
+require "pry"
+
+def largest_prime_factor(value)
+  reduced_value = Math.sqrt(value)
+  old_prime = nil
+  Prime.each(reduced_value) do |prime|
+    if (prime<value && value%prime==0)
+      old_prime = prime
+    end
+  end
+  return old_prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,19 @@
-# Enter your object-oriented solution here!
+require "prime"
+require "pry"
+
+class LargestPrimeFactor
+  def initialize(value_int)
+    @value = value_int
+  end
+
+  def number()
+    reduced_value = Math.sqrt(@value)
+    old_prime = nil
+    Prime.each(reduced_value) do |prime|
+      if (prime<@value && @value%prime==0)
+        old_prime = prime
+      end
+    end
+    return old_prime
+  end
+end
